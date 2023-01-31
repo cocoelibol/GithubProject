b134bf1561b0ad907ef837dfc392729d12783762
coco <cocoelibol@gmail.com>
2023-01-30 21:46:49 -0500

    I added a new class named thirdgithubclass

--- src/Day01GitHub/ClassGithub.java
+++ src/Day01GitHub/ClassGithub.java
@@ -2,6 +2,6 @@ package Day01GitHub;
 
 public class ClassGithub {
     public static void main(String[] args) {
-        System.out.println("Hey!");
+        System.out.println("i am writing this on CansuFeature");
     }
 }
